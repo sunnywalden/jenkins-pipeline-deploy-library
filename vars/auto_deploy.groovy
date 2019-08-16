4334d85569bc19ee17371ac7a2ed3c27cd3020b2
zhangbo <zhangbo@tezign.com>
2019-08-16 16:56:25 +0800

    fix

--- vars/auto_deploy.groovy
+++ vars/auto_deploy.groovy
@@ -31,267 +31,270 @@ def send_all(file_str) {
     }
 }
 
+
 def call(String type, Map map) {
-    if ( type == "maven" ) {
-        pipeline {
-        agent {
-
-            docker {
-                image 'maven:3-alpine'
-                    args "${map.BUILD_ARGS}"
-            }
-        }
-
-        environment {
-            // Ansible host
-            REMOTE_HOST = "${map.REMOTE_HOST}"
-            REMOTE_USER = "${map.REMOTE_USER}"
-            REMOTE_PORT = "${map.REMOTE_PORT}"
-            REMOTE_SUDO_PASSWORD = "${map.REMOTE_SUDO_PASSWORD}"
-            //  git config
-            REPO_URL = "${map.REPO_URL}"
-            BRANCH_NAME = "${map.BRANCH_NAME}"
-            CREDENTIALS_ID = 'artifactory'
-            TAG = "${map.tag}"
-
-            // env type
-            ENV_TYPE = "${map.ENV_TYPE}"
-
-            // build config
-            // values in: npm maven or none
-            BUILD_TYPE = "${map.BUILD_TYPE}"
-            BUILD_ARGS = "${map.BUILD_ARGS}"
-            BUILD_CMD = "${map.BUILD_CMD}"
-
-
-            // docker config
-            REGISTRY_URL = "${map.REGISTRY_URL}"
-            MEMORY_LIMIT = "${map.MEMORY_LIMIT}"
-            PORTS = "${map.PORTS}"
-            REPLICATES = "${map.REPLICATES}"
-            HEALTH_CHECK = "${map.HEALTH_CHECK}"
-            NETWORK = "${map.NETWORK}"
-            VOLUMES = "${map.VOLUMES}"
-            ENVS = "${map.ENVS}"
-
-            // deploy config
-            APP_NAME = "${map.APP_NAME}"
-            IMAGE_NAME = "${REGISTRY_URL}/" + "${map.APP_NAME}" + "_${map.ENV_TYPE}"
-            STACK_FILE_NAME = 'docker-stack.yml'
-            SEND_FILES = "${map.SEND_FILES}"
-        }
-
-        // cron for pipe
-//         triggers {
-//         cron('H */4 * * 1-5')
+//     if ( type == "maven" ) {
+//         pipeline {
+//         agent {
+//
+//             docker {
+//                 image 'maven:3-alpine'
+//                     args "${map.BUILD_ARGS}"
+//             }
 //         }
-
-        stages {
-            stage('获取代码') {
-
-                steps {
-                    git([url: "${REPO_URL}", branch: "${BRANCH_NAME}", credentialsId: "${CREDENTIALS_ID}"])
-                }
-            }
-
-            stage('编译代码') {
-                steps {
-                    sh 'echo `pwd`'
-                    sh "${BUILD_CMD}"
-                }
-            }
-
-            stage('构建镜像') {
-                steps {
-                    sh "docker build -t ${IMAGE_NAME}:${env.BUILD_ID} ."
-                    sh "docker push ${IMAGE_NAME}:${env.BUILD_ID}"
-                }
-            }
-
-            stage('获取主机') {
-                steps {
-                    script {
-                        remote = getServer()
-                    }
-                }
-            }
-
-            stage('更新') {
-                when {
-                    expression {
-                        SEND_FILES != []
-                    }
-                }
-                steps {
-                    echo "print all files objects: ${SEND_FILES}"
-                    // send files
-                    send_all("${SEND_FILES}")
-                }
-            }
-
-            stage('发布') {
-                steps {
-                    // generate deploy script
-                    writeFile file: 'deploy.sh', text: "wget -O ${STACK_FILE_NAME} " +
-                        " https://git.tezign.com/ops/jenkins-script/raw/master/resources/docker-compose/${STACK_FILE_NAME} \n" +
-                        "sudo docker stack deploy -c /tmp/${STACK_FILE_NAME} ${APP_NAME}"
-
-                    // deploy
-                    sshPut remote: remote, from: "${STACK_FILE_NAME}", into: "/tmp"
-                    sshPut remote: remote, from: "${STACK_FILE_NAME}", into: "/tmp"
-                    sshScript remote: remote, script: "deploy.sh"
-                }
-            }
-        }
-        post {
-            always {
-                echo 'Deploy pipeline finished'
-            }
-            failure {
-                mail to: 'zhangbo@tezign.com', subject: 'The Pipeline failed', body: '''
-                    Deploy pipeline is failed
-                '''
-            }
-        }
-    }
-    }
-    else if ( type == "npm" ) {
-            pipeline {
-//         agent none
+//
+//         environment {
+//             // Ansible host
+//             REMOTE_HOST = "${map.REMOTE_HOST}"
+//             REMOTE_USER = "${map.REMOTE_USER}"
+//             REMOTE_PORT = "${map.REMOTE_PORT}"
+//             REMOTE_SUDO_PASSWORD = "${map.REMOTE_SUDO_PASSWORD}"
+//             //  git config
+//             REPO_URL = "${map.REPO_URL}"
+//             BRANCH_NAME = "${map.BRANCH_NAME}"
+//             CREDENTIALS_ID = 'artifactory'
+//             TAG = "${map.tag}"
+//
+//             // env type
+//             ENV_TYPE = "${map.ENV_TYPE}"
+//
+//             // build config
+//             // values in: npm maven or none
+//             BUILD_TYPE = "${map.BUILD_TYPE}"
+//             BUILD_ARGS = "${map.BUILD_ARGS}"
+//             BUILD_CMD = "${map.BUILD_CMD}"
+//
+//
+//             // docker config
+//             REGISTRY_URL = "${map.REGISTRY_URL}"
+//             MEMORY_LIMIT = "${map.MEMORY_LIMIT}"
+//             PORTS = "${map.PORTS}"
+//             REPLICATES = "${map.REPLICATES}"
+//             HEALTH_CHECK = "${map.HEALTH_CHECK}"
+//             NETWORK = "${map.NETWORK}"
+//             VOLUMES = "${map.VOLUMES}"
+//             ENVS = "${map.ENVS}"
+//
+//             // deploy config
+//             APP_NAME = "${map.APP_NAME}"
+//             IMAGE_NAME = "${REGISTRY_URL}/" + "${map.APP_NAME}" + "_${map.ENV_TYPE}"
+//             STACK_FILE_NAME = 'docker-stack.yml'
+//             SEND_FILES = "${map.SEND_FILES}"
+//         }
+//
+//         // cron for pipe
+// //         triggers {
+// //         cron('H */4 * * 1-5')
+// //         }
+//
+//         stages {
+//             stage('获取代码') {
+//
+//                 steps {
+//                     git([url: "${REPO_URL}", branch: "${BRANCH_NAME}", credentialsId: "${CREDENTIALS_ID}"])
+//                 }
+//             }
+//
+//             stage('编译代码') {
+//                 steps {
+//                     sh 'echo `pwd`'
+//                     sh "${BUILD_CMD}"
+//                 }
+//             }
+//
+//             stage('构建镜像') {
+//                 steps {
+//                     sh "docker build -t ${IMAGE_NAME}:${env.BUILD_ID} ."
+//                     sh "docker push ${IMAGE_NAME}:${env.BUILD_ID}"
+//                 }
+//             }
+//
+//             stage('获取主机') {
+//                 steps {
+//                     script {
+//                         remote = getServer()
+//                     }
+//                 }
+//             }
+//
+//             stage('更新') {
+//                 when {
+//                     expression {
+//                         SEND_FILES != []
+//                     }
+//                 }
+//                 steps {
+//                     echo "print all files objects: ${SEND_FILES}"
+//                     // send files
+//                     send_all("${SEND_FILES}")
+//                 }
+//             }
+//
+//             stage('发布') {
+//                 steps {
+//                     // generate deploy script
+//                     writeFile file: 'deploy.sh', text: "wget -O ${STACK_FILE_NAME} " +
+//                         " https://git.tezign.com/ops/jenkins-script/raw/master/resources/docker-compose/${STACK_FILE_NAME} \n" +
+//                         "sudo docker stack deploy -c /tmp/${STACK_FILE_NAME} ${APP_NAME}"
+//
+//                     // deploy
+//                     sshPut remote: remote, from: "${STACK_FILE_NAME}", into: "/tmp"
+//                     sshPut remote: remote, from: "${STACK_FILE_NAME}", into: "/tmp"
+//                     sshScript remote: remote, script: "deploy.sh"
+//                 }
+//             }
+//         }
+//         post {
+//             always {
+//                 echo 'Deploy pipeline finished'
+//             }
+//             failure {
+//                 mail to: 'zhangbo@tezign.com', subject: 'The Pipeline failed', body: '''
+//                     Deploy pipeline is failed
+//                 '''
+//             }
+//         }
+//     }
+//     }
+//     else if ( type == "npm" ) {
+//             pipeline {
+// //         agent none
 //         agent {
 //             label 'master'
 //         }
-        agent {
-
-            docker {
-                image 'node:6-alpine'
-                    args "${map.BUILD_ARGS}"
-            }
-        }
-
-        environment {
-            // Ansible host
-            REMOTE_HOST = "${map.REMOTE_HOST}"
-            REMOTE_USER = "${map.REMOTE_USER}"
-            REMOTE_PORT = "${map.REMOTE_PORT}"
-            REMOTE_SUDO_PASSWORD = "${map.REMOTE_SUDO_PASSWORD}"
-            //  git config
-            REPO_URL = "${map.REPO_URL}"
-            BRANCH_NAME = "${map.BRANCH_NAME}"
-            CREDENTIALS_ID = 'artifactory'
-            TAG = "${map.tag}"
-
-            // env type
-            ENV_TYPE = "${map.ENV_TYPE}"
-
-            // build config
-            // values in: npm maven or none
-            BUILD_TYPE = "${map.BUILD_TYPE}"
-            BUILD_ARGS = "${map.BUILD_ARGS}"
-            BUILD_CMD = "${map.BUILD_CMD}"
-
-
-            // docker config
-            REGISTRY_URL = "${map.REGISTRY_URL}"
-            MEMORY_LIMIT = "${map.MEMORY_LIMIT}"
-            PORTS = "${map.PORTS}"
-            REPLICATES = "${map.REPLICATES}"
-            HEALTH_CHECK = "${map.HEALTH_CHECK}"
-            NETWORK = "${map.NETWORK}"
-            VOLUMES = "${map.VOLUMES}"
-            ENVS = "${map.ENVS}"
-
-            // deploy config
-            APP_NAME = "${map.APP_NAME}"
-            IMAGE_NAME = "${REGISTRY_URL}/" + "${map.APP_NAME}" + "_${map.ENV_TYPE}"
-            STACK_FILE_NAME = 'docker-stack.yml'
-            SEND_FILES = "${map.SEND_FILES}"
-        }
-
-        // cron for pipe
-//         triggers {
-//         cron('H */4 * * 1-5')
+// //         agent {
+// //
+// //             docker {
+// //                 image 'node:6-alpine'
+// //                     args "${map.BUILD_ARGS}"
+// //             }
+// //         }
+//
+//         environment {
+//             // Ansible host
+//             REMOTE_HOST = "${map.REMOTE_HOST}"
+//             REMOTE_USER = "${map.REMOTE_USER}"
+//             REMOTE_PORT = "${map.REMOTE_PORT}"
+//             REMOTE_SUDO_PASSWORD = "${map.REMOTE_SUDO_PASSWORD}"
+//             //  git config
+//             REPO_URL = "${map.REPO_URL}"
+//             BRANCH_NAME = "${map.BRANCH_NAME}"
+//             CREDENTIALS_ID = 'artifactory'
+//             TAG = "${map.tag}"
+//
+//             // env type
+//             ENV_TYPE = "${map.ENV_TYPE}"
+//
+//             // build config
+//             // values in: npm maven or none
+//             BUILD_TYPE = "${map.BUILD_TYPE}"
+//             BUILD_ARGS = "${map.BUILD_ARGS}"
+//             BUILD_CMD = "${map.BUILD_CMD}"
+//
+//
+//             // docker config
+//             REGISTRY_URL = "${map.REGISTRY_URL}"
+//             MEMORY_LIMIT = "${map.MEMORY_LIMIT}"
+//             PORTS = "${map.PORTS}"
+//             REPLICATES = "${map.REPLICATES}"
+//             HEALTH_CHECK = "${map.HEALTH_CHECK}"
+//             NETWORK = "${map.NETWORK}"
+//             VOLUMES = "${map.VOLUMES}"
+//             ENVS = "${map.ENVS}"
+//
+//             // deploy config
+//             APP_NAME = "${map.APP_NAME}"
+//             IMAGE_NAME = "${REGISTRY_URL}/" + "${map.APP_NAME}" + "_${map.ENV_TYPE}"
+//             STACK_FILE_NAME = 'docker-stack.yml'
+//             SEND_FILES = "${map.SEND_FILES}"
 //         }
-
-        stages {
-            stage('获取代码') {
-
-                steps {
-                    git([url: "${REPO_URL}", branch: "${BRANCH_NAME}", credentialsId: "${CREDENTIALS_ID}"])
-                }
-            }
-
-            stage('编译代码') {
-                steps {
-                    sh 'echo `pwd`'
-                    sh "${BUILD_CMD}"
-                }
-            }
-
-            stage('构建镜像') {
-                steps {
-                    sh "docker build -t ${IMAGE_NAME}:${env.BUILD_ID} ."
-                    sh "docker push ${IMAGE_NAME}:${env.BUILD_ID}"
-                }
-            }
-
-            stage('获取主机') {
-                steps {
-                    script {
-                        remote = getServer()
-                    }
-                }
-            }
-
-            stage('更新') {
-                when {
-                    expression {
-                        SEND_FILES != []
-                    }
-                }
-                steps {
-                    echo "print all files objects: ${SEND_FILES}"
-                    // send files
-                    send_all("${SEND_FILES}")
-                }
-            }
-
-            stage('发布') {
-                steps {
-                    // generate deploy script
-                    writeFile file: 'deploy.sh', text: "wget -O ${STACK_FILE_NAME} " +
-                        " https://git.tezign.com/ops/jenkins-script/raw/master/resources/docker-compose/${STACK_FILE_NAME} \n" +
-                        "sudo docker stack deploy -c /tmp/${STACK_FILE_NAME} ${APP_NAME}"
-
-                    // deploy
-                    sshPut remote: remote, from: "${STACK_FILE_NAME}", into: "/tmp"
-                    sshPut remote: remote, from: "${STACK_FILE_NAME}", into: "/tmp"
-                    sshScript remote: remote, script: "deploy.sh"
-                }
-            }
-        }
-        post {
-            always {
-                echo 'Deploy pipeline finished'
-            }
-            failure {
-                mail to: 'zhangbo@tezign.com', subject: 'The Pipeline failed', body: '''
-                    Deploy pipeline is failed
-                '''
-            }
-        }
-    }
-    }
-    else {
+//
+//         // cron for pipe
+// //         triggers {
+// //         cron('H */4 * * 1-5')
+// //         }
+//
+//         stages {
+//             stage('获取代码') {
+//
+//                 steps {
+//                     git([url: "${REPO_URL}", branch: "${BRANCH_NAME}", credentialsId: "${CREDENTIALS_ID}"])
+//                 }
+//             }
+//
+//             stage('编译代码') {
+//                 steps {
+//                     sh 'echo `pwd`'
+//                     sh "${BUILD_CMD}"
+//                 }
+//             }
+//
+//             stage('构建镜像') {
+//                 steps {
+//                     sh "docker build -t ${IMAGE_NAME}:${env.BUILD_ID} ."
+//                     sh "docker push ${IMAGE_NAME}:${env.BUILD_ID}"
+//                 }
+//             }
+//
+//             stage('获取主机') {
+//                 steps {
+//                     script {
+//                         remote = getServer()
+//                     }
+//                 }
+//             }
+//
+//             stage('更新') {
+//                 when {
+//                     expression {
+//                         SEND_FILES != []
+//                     }
+//                 }
+//                 steps {
+//                     echo "print all files objects: ${SEND_FILES}"
+//                     // send files
+//                     send_all("${SEND_FILES}")
+//                 }
+//             }
+//
+//             stage('发布') {
+//                 steps {
+//                     // generate deploy script
+//                     writeFile file: 'deploy.sh', text: "wget -O ${STACK_FILE_NAME} " +
+//                         " https://git.tezign.com/ops/jenkins-script/raw/master/resources/docker-compose/${STACK_FILE_NAME} \n" +
+//                         "sudo docker stack deploy -c /tmp/${STACK_FILE_NAME} ${APP_NAME}"
+//
+//                     // deploy
+//                     sshPut remote: remote, from: "${STACK_FILE_NAME}", into: "/tmp"
+//                     sshPut remote: remote, from: "${STACK_FILE_NAME}", into: "/tmp"
+//                     sshScript remote: remote, script: "deploy.sh"
+//                 }
+//             }
+//         }
+//         post {
+//             always {
+//                 echo 'Deploy pipeline finished'
+//             }
+//             failure {
+//                 mail to: 'zhangbo@tezign.com', subject: 'The Pipeline failed', body: '''
+//                     Deploy pipeline is failed
+//                 '''
+//             }
+//         }
+//     }
+//     }
+//     else {
     pipeline {
         agent {
-
-            docker {
-//                 image 'python:3-alpine'
-                image 'docker'
-                args "${map.BUILD_ARGS}"
-            }
+            label "master"
         }
+//         agent {
+//
+//             docker {
+//                 image 'docker'
+//                 args "${map.BUILD_ARGS}"
+//             }
+//         }
 
         environment {
             // Ansible host
@@ -331,11 +334,6 @@ def call(String type, Map map) {
             STACK_FILE_NAME = 'docker-stack.yml'
             SEND_FILES = "${map.SEND_FILES}"
         }
-
-        // cron for pipe
-//         triggers {
-//         cron('H */4 * * 1-5')
-//         }
 
         stages {
             stage('获取代码') {
@@ -388,8 +386,7 @@ def call(String type, Map map) {
                         "sudo docker stack deploy -c /tmp/${STACK_FILE_NAME} ${APP_NAME}"
 
                     // deploy
-                    sshPut remote: remote, from: "${STACK_FILE_NAME}", into: "/tmp"
-                    sshPut remote: remote, from: "${STACK_FILE_NAME}", into: "/tmp"
+                    sshPut remote: remote, from: "../resources/dcoker-compose/docker-stack.groovy", into: "/tmp/docker-stack.yml"
                     sshScript remote: remote, script: "deploy.sh"
                 }
             }
@@ -405,5 +402,5 @@ def call(String type, Map map) {
             }
         }
     }
-    }
+//     }
 }
