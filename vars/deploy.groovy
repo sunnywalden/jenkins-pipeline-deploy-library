0935f24aa6a4ae4fe6419f2b62e21b231963c4ff
zhangbo <zhangbo@tezign.com>
2019-08-16 18:28:06 +0800

    position common func in src

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,8 +1,89 @@
 #!groovy
 
 import groovy.json.JsonSlurper
-import src.org.deploy.Utils.ComFunc
-import src.org.deploy.Compose.DockerCompose
+// import src.org.deploy.Utils.ComFunc
+// import src.org.deploy.Compose.DockerCompose
+
+def generate_compose() {
+        filePath = "/tmp/docker-stack.yml"
+        File file = new File(filePath)
+
+        file <<
+"""
+version: '3.4'
+services:
+    "{{ APP_NAME }}":
+         image: "{{ IMAGE_NAME }}:{{ env.BUILD_ID }}"
+         {% if PORTS != '' -%}
+         ports:
+           - '{{ PORTS }}'
+         {% endif %}
+
+         environment: '{{ ENVS }}'
+
+         networks:
+           - '{{ NETWORK }}'
+         volumes: '{{ VOLUMES }}'
+         stop_grace_period: 30s # Specify how long to wait when attempting to stop a container if it doesn’t handle SIGTERM
+         deploy:
+           replicas: '{{ REPLICATES }}'
+           resources:
+             limits:
+               memory: "{{ MEMORY_LIMIT }}"
+           update_config:
+             parallelism: 1            # The number of containers to update at a time.
+             delay: 0s                 # The time to wait between updating a group of containers.
+             failure_action: rollback  # What to do if an update fails  One of continue, rollback, or pause (default: pause).
+             max_failure_ratio: 0      # Failure rate to tolerate during an update.
+             order: start-first
+           restart_policy:
+             condition: any
+             max_attempts: 3
+         healthcheck:
+           test: "{{ HEALTH_CHECK }}"
+           interval: 3s
+           timeout: 5s
+           retries: 3
+           start_period: 2m
+# external.name was deprecated in version 3.5 file format use name instead.
+# https://docs.docker.com/compose/compose-file/#external-1
+networks:
+  tezign:
+    external: true
+"""
+    println file.text
+}
+
+
+def getServer() {
+    def remote = [:]
+    remote.name = 'manager node'
+    remote.user = "${REMOTE_USER}"
+    remote.host = "${REMOTE_HOST}"
+    remote.password = "${REMOTE_SUDO_PASSWORD}"
+    remote.port = "${REMOTE_PORT}".toInteger()
+    remote.identityFile = '/root/.ssh/id_rsa'
+    remote.allowAnyHosts = true
+    return remote
+}
+
+def send_all(file_str) {
+
+    files_list = file_str.split(',')
+    files_list.each { item ->
+            echo "print file object to be send: ${item}"
+            files = item.split(':')
+            if (files.size() == 2) {
+                source_file = files[0]
+                dest_file = files[1]
+            } else {
+                return 0
+            }
+            echo "send file ${source_file} to ${dest_file}"
+            sshPut remote: remote, from: "${source_file}", into: "${dest_file}"
+    }
+}
+
 
 def call(String type, Map map) {
 //     if ( type == "maven" ) {
